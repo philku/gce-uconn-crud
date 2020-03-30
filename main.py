939e2817a3d6d62cf2a731dd0c3d7bc10969694c
philku <pkubi0001@mail.ct.edu>
2020-03-29 23:53:12 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,100 @@
+# Required Flask Libraries
+from flask import Flask, request, render_template, redirect
+
+# Imports the Google Cloud client library
+from google.cloud import datastore
+client = datastore.Client()
+kind = 'Custinfo'
+
+# Start Flask app
+app = Flask(__name__)
+
+
+# Index Page
+@app.route('/', methods=['kind'])
+def index():
+    # Get Customer List
+    query = client.query(kind=kind)
+    results = list(query.fetch())
+
+    # Render index page
+    return render_template('index.html', customers=results)
+
+
+# CRUD ENDPOINTS
+# Create
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)      # Data from form
+
+        # Put customer record
+        complete_key = client.key(kind, data['name'])
+        customer = datastore.Entity(key=complete_key)
+        customer.update({
+            'Name': data['Name'],
+            'address': data['address'],
+            'instructions': data['instructions'],
+            'address_type': data['address_type']
+        })
+        client.put(customer)
+
+        # Redirect to customer page
+        return redirect("/" + data['Name'])
+    else:
+        # GET - Render customer creation form
+        return render_template('create.html')
+
+
+# Read
+@app.route('/<name>', methods=['GET'])
+def read(name):
+    # Retrieve Customer Data
+    key = client.key(kind, name)
+    customer = client.get(key)
+
+    # Render the page
+    return render_template('customer.html', name=customer['Name'], address=customer['address'],
+                           instructions=customer['instructions'], address_type=customer['address_type'])
+
+
+# Update
+@app.route('/<name>/update', methods=['GET', 'POST'])
+def update(name):
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)      # Data from form
+
+        # Update Customer Data
+        key = client.key(kind, name)
+        customer = client.get(key)
+        customer['address'] = data['address']
+        customer['instructions'] = data['instructions']
+        customer['address_type'] = data['address_type']
+
+        # Redirect to customer page
+        return redirect("/" + name)
+
+    else:
+        # Get customer data
+        key = client.key(kind, name)
+        customer = client.get(key)
+
+        # Renders update page with existing data
+        return render_template('update.html', name=customer['Name'], address=customer['address'],
+                               instructions=customer['instructions'], address_type=customer['address_type'])
+
+
+# Delete
+@app.route('/<name>/delete', methods=['GET'])
+def delete(name):
+    # Delete Customer Record
+    key = client.key(kind, name)
+    client.delete(key)
+
+    # Redirect to index page
+    return redirect('/')
+
+
+# Don't worry about this part
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
---
+++ readme.md
@@ -0,0 +1,35 @@
+# Sample CRUD App
+This is a sample CRUD (Create, Read, Update, and Delete) application 
+that runs on Google App Engine using Python and Datastore. 
+
+## Overview
+
+### Files
+This application uses the following main file structure:
+
+gce-uconn-crud                         <br>
+├─ requirements.txt                       <br>
+├─ app.yaml                               <br>
+├─ main.py                                <br>
+├─ templates                                <br>
+│  ├── index.html                       <br>
+│  ├── customer.html                       <br>
+│  ├── create.html                       <br>
+│  ├── update.html                       
+
+#### requirements.txt
+A list of the required packages for the app
+
+#### app.yaml
+A config file for google app engine
+
+#### main.py
+The main application
+
+#### templates
+HTML files used in the app
+
+
+## Setup
+
+### 1
